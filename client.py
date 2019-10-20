e264b631b26895c6201008059fe2b5f32d428bc7
Matheus <matheus.carvalho96@gmail.com>
2019-10-20 14:05:30 -0300

    Changed: host e port configurados

--- client.py
+++ client.py
@@ -1,43 +1,43 @@
 #!/usr/bin/env python3
-"""Script for Tkinter GUI chat client."""
+
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
 
-
+# Configura o recebimento das mensagens.
 def receive():
-    """Handles receiving of messages."""
     while True:
         try:
             msg = client_socket.recv(BUFSIZ).decode("utf8")
             msg_list.insert(tkinter.END, msg)
-        except OSError:  # Possibly client has left the chat.
+        except OSError:  # O cliente saiu do chat.
             break
 
 
-def send(event=None):  # event is passed by binders.
-    """Handles sending of messages."""
+# Configura o envio das mensagens.
+def send(event=None):
     msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
+    my_msg.set("")  # Limpa o input do texto.
     client_socket.send(bytes(msg, "utf8"))
     if msg == "{quit}":
         client_socket.close()
         top.quit()
 
 
+# Função para fechar a tela.
 def on_closing(event=None):
-    """This function is to be called when the window is closed."""
     my_msg.set("{quit}")
     send()
 
 top = tkinter.Tk()
-top.title("Chatter")
+top.title("Chat c/ Socket - Programação Distribuída e Paralela")
 
 messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
+my_msg = tkinter.StringVar()  # Para enviar as mensagens.
 my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# Following will contain the messages.
+scrollbar = tkinter.Scrollbar(messages_frame)  # Para navegar entre as mensagens.
+
+# Histórico das mensagens.
 msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
@@ -47,14 +47,19 @@ messages_frame.pack()
 entry_field = tkinter.Entry(top, textvariable=my_msg)
 entry_field.bind("<Return>", send)
 entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
+send_button = tkinter.Button(top, text="Enviar", command=send)
 send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-#----Now comes the sockets part----
-HOST = input('Enter host: ')
-PORT = input('Enter port: ')
+#----Configuração do socket----
+HOST = input('Entre com o host (padrão: localhost): ')
+PORT = input('Entre com a porta (padrão: 33000): ')
+if not HOST:
+    HOST = "localhost"
+else:
+    HOST = HOST
+
 if not PORT:
     PORT = 33000
 else:
@@ -68,4 +73,4 @@ client_socket.connect(ADDR)
 
 receive_thread = Thread(target=receive)
 receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+tkinter.mainloop()
